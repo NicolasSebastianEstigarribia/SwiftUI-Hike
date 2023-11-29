0b9c57f3e76580d2c09f460709a5a3a5058c4a91
Nicolas Estigarribia <nicolas.sebastian.estigarribia@gmail.com>
2023-11-29 08:30:13 -0300

    Random Coordinate

--- Hike/Component/MotionAnimationView.swift
+++ Hike/Component/MotionAnimationView.swift
@@ -8,11 +8,40 @@
 import SwiftUI
 
 struct MotionAnimationView: View {
+    // MARK: PROPERTIES
+    @State private var randomCircle: Int = Int.random(in: 6...12)
+    
+    // MARK: FUNCTIONS
+    
+    // 1. RANDOM COORDINATE
+    func randomCoordinate() -> CGFloat {
+        return CGFloat.random(in: 0...256)
+    }
+    // 2. RANDOM SIZE
+    // 3. RANDOM SCALE
+    // 4. RANDOM SPEED
+    // 5. RANDOM DELAY
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            ForEach(0...randomCircle, id: \.self) { item in
+                Circle()
+                    .foregroundColor(.white)
+                    .opacity(0.25)
+                    .position(
+                        x: randomCoordinate(),
+                        y: randomCoordinate()
+                    )
+            }
+        } //: ZSTACK
+        .frame(width: 256, height: 256)
     }
 }
 
 #Preview {
-    MotionAnimationView()
+    ZStack {
+        Color.teal.ignoresSafeArea()
+        MotionAnimationView()
+    }
 }
