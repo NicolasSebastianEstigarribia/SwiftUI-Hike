99c65b2ac1e9e565be98ccce17697b15ce66af56
Nicolas Estigarribia <nicolas.sebastian.estigarribia@gmail.com>
2023-11-30 07:41:42 -0300

    Motion Animation View

--- Hike/Component/CustomCircleView.swift
+++ Hike/Component/CustomCircleView.swift
@@ -11,23 +11,27 @@ struct CustomCircleView: View {
     @State private var isAnimateGradient:Bool = false
     
     var body: some View {
-        Circle().fill(
-            LinearGradient(colors:
-                [
-                    .colorIndigoMedium,
-                    .colorSalmonLight
-                ],
-               startPoint: isAnimateGradient ? .topLeading : .bottomLeading,
-               endPoint: isAnimateGradient ? .bottomTrailing : .topTrailing
-                )
-        )
-        .onAppear {
-            withAnimation(
-                .linear(duration: 3.0)
-                .repeatForever(autoreverses: true)
-            ) {
-                isAnimateGradient.toggle()
+        ZStack {
+            Circle()
+                .fill(
+                LinearGradient(colors:
+                    [
+                        .colorIndigoMedium,
+                        .colorSalmonLight
+                    ],
+                   startPoint: isAnimateGradient ? .topLeading : .bottomLeading,
+                   endPoint: isAnimateGradient ? .bottomTrailing : .topTrailing
+                    )
+            )
+            .onAppear {
+                withAnimation(
+                    .linear(duration: 3.0)
+                    .repeatForever(autoreverses: true)
+                ) {
+                    isAnimateGradient.toggle()
+                }
             }
+            MotionAnimationView()
         }
         .frame(width: 256, height: 256)
     }
--- Hike/Component/MotionAnimationView.swift
+++ Hike/Component/MotionAnimationView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct MotionAnimationView: View {
     // MARK: PROPERTIES
     @State private var randomCircle: Int = Int.random(in: 6...12)
+    @State private var isAnimating: Bool = false
     
     // MARK: FUNCTIONS
     
@@ -21,10 +22,18 @@ struct MotionAnimationView: View {
     func randomSize() -> CGFloat {
         return CGFloat.random(in: 4...80)
     }
-    
     // 3. RANDOM SCALE
+    func randomScale() -> CGFloat {
+        return CGFloat(Double.random(in: 0.1...2.0))
+    }
     // 4. RANDOM SPEED
+    func randomSpeed() -> Double {
+        return Double.random(in: 0.05...1.0)
+    }
     // 5. RANDOM DELAY
+    func randomDelay() -> Double {
+        return Double.random(in: 0...2)
+    }
     
     
     var body: some View {
@@ -38,15 +47,31 @@ struct MotionAnimationView: View {
                         x: randomCoordinate(),
                         y: randomCoordinate()
                     )
+                    .scaleEffect(isAnimating ? randomScale() : 1)
+                    .onAppear {
+                        withAnimation(
+                            .interpolatingSpring(stiffness: 0.25, damping: 0.25)
+                            .repeatForever()
+                            .speed(randomSpeed())
+                            .delay(randomDelay())
+                        ) {
+                            isAnimating = true
+                        }
+                    }
+
+
             }
         } //: ZSTACK
         .frame(width: 256, height: 256)
+        .mask(Circle())
+        .drawingGroup()
     }
 }
 
 #Preview {
-    ZStack {
-        Color.teal.ignoresSafeArea()
-        MotionAnimationView()
-    }
+    MotionAnimationView()
+        .background(
+            Circle()
+                .foregroundColor(.teal)
+        )
 }
