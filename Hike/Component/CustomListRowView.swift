00de810cb6c1e8f091907a50f16a39fd659881d6
Nicolas Estigarribia <nicolas.sebastian.estigarribia@gmail.com>
2024-01-15 07:53:01 -0300

    Custom ListRow with labeledContent

---
+++ Hike/Component/CustomListRowView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomListRowView.swift
+//  Hike
+//
+//  Created by Nicolas Estigarribia on 15/01/2024.
+//
+
+import SwiftUI
+
+struct CustomListRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomListRowView()
+}
